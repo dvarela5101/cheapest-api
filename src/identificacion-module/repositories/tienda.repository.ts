fe5166f294c1bf6a077d36ff1bd5a62e17301004
dvarela5101 <d.varela1@uniandes.edu.co>
2026-08-23 21:44:49 -0500

    correciones finales repositorio y servicios

--- src/identificacion-module/repositories/tienda.repository.ts
+++ src/identificacion-module/repositories/tienda.repository.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { QueryTiendaDto } from '../dtos';
-import { EstadoCaptacion, Tienda } from './entities';
+import { Tienda } from './entities';
 
 @Injectable()
 export class TiendaRepository {
@@ -11,25 +11,30 @@ export class TiendaRepository {
   ) {}
 
   async create(tienda: Partial<Tienda>): Promise<Tienda> {
-    const newTienda = this.repository.create(tienda)
+    const newTienda = this.repository.create(tienda);
     return this.repository.save(newTienda);
   }
 
   async findAll(query: QueryTiendaDto): Promise<Tienda[]> {
-    const queryBuilder = this.repository.createQueryBuilder('tienda')
+    const queryBuilder = this.repository.createQueryBuilder('tienda');
 
+    if (query.paisId) {
+      queryBuilder.andWhere('tienda.paisId = :paisId', {
+        paisId: query.paisId,
+      });
+    }
 
-    if(query.paisId){
-      queryBuilder.andWhere('tienda.paisId = :paisId',{ paisId :  query.paisId})
+    if (query.estadoCaptacion) {
+      queryBuilder.andWhere('tienda.estadoCaptacion = :estadoCaptacion', {
+        estadoCaptacion: query.estadoCaptacion,
+      });
     }
-    if(query.estadoCaptacion){
-      queryBuilder.andWhere('tienda.estadoCaptacion = :estatoCaptacion',{ EstadoCaptacion :  query.estadoCaptacion})
-    }
+
     return queryBuilder.getMany();
   }
 
   async findById(id: string): Promise<Tienda | null> {
-    return this.repository.findOne({where:{id}});
+    return this.repository.findOne({ where: { id } });
   }
 
   async update(id: string, updates: Partial<Tienda>): Promise<Tienda | null> {
@@ -38,7 +43,7 @@ export class TiendaRepository {
   }
 
   async delete(id: string): Promise<boolean> {
-    const result = await this.repository.delete(id)
-    return (result.affected ?? 0 )> 0 
+    const result = await this.repository.delete(id);
+    return (result.affected ?? 0) > 0;
   }
 }
--- src/identificacion-module/services/tienda.service.ts
+++ src/identificacion-module/services/tienda.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import {
   CreateTiendaDto,
   QueryTiendaDto,
@@ -13,32 +13,55 @@ export class TiendaService {
   constructor(private readonly tiendaRepository: TiendaRepository) {}
 
   async create(dto: CreateTiendaDto): Promise<TiendaResponseDto> {
-    // TODO: implementar
-    throw new Error('Not implemented');
+    const tienda = await this.tiendaRepository.create(dto);
+    return this.mapToResponse(tienda);
   }
 
   async findAll(query: QueryTiendaDto): Promise<TiendaResponseDto[]> {
-    // TODO: implementar
-    throw new Error('Not implemented');
+    const tiendas = await this.tiendaRepository.findAll(query);
+    return tiendas.map((tienda) => this.mapToResponse(tienda));
   }
 
   async findById(id: string): Promise<TiendaResponseDto> {
-    // TODO: implementar
-    throw new Error('Not implemented');
+    const tienda = await this.tiendaRepository.findById(id);
+    if (!tienda) {
+      throw new NotFoundException(`Tienda con id ${id} no encontrada`);
+    }
+    return this.mapToResponse(tienda);
   }
 
   async update(id: string, dto: UpdateTiendaDto): Promise<TiendaResponseDto> {
-    // TODO: implementar
-    throw new Error('Not implemented');
+    const tienda = await this.tiendaRepository.findById(id);
+    if (!tienda) {
+      throw new NotFoundException(`Tienda con id ${id} no encontrada`);
+    }
+
+    const updatedTienda = await this.tiendaRepository.update(id, dto);
+    return this.mapToResponse(updatedTienda!);
   }
 
   async delete(id: string): Promise<void> {
-    // TODO: implementar
-    throw new Error('Not implemented');
+    const tienda = await this.tiendaRepository.findById(id);
+    if (!tienda) {
+      throw new NotFoundException(`Tienda con id ${id} no encontrada`);
+    }
+
+    await this.tiendaRepository.delete(id);
   }
 
   private mapToResponse(tienda: Tienda): TiendaResponseDto {
-    // TODO: implementar
-    throw new Error('Not implemented');
+    return {
+      id: tienda.id,
+      codigoInterno: tienda.codigoInterno,
+      nombreComercial: tienda.nombreComercial,
+      responsableNombre: tienda.responsableNombre,
+      paisId: tienda.paisId,
+      rut: tienda.rut,
+      direccion: tienda.direccion,
+      telefono: tienda.telefono,
+      estadoCaptacion: tienda.estadoCaptacion,
+      createdAt: tienda.createdAt,
+      updatedAt: tienda.updatedAt,
+    };
   }
 }
